4bb6086e3cbe65d141f480e86fcebd073a2b4b4b
Antonio Santana <antcesar.santana@gmail.com>
2017-02-21 10:45:18 -0300

    adicionando o console.log para mostrar a  rota

--- src/App.js
+++ src/App.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import Geosuggest from 'react-geosuggest';
 
 class App extends Component {
+  onFocus () {
+    console.log('onFocus');
+  }
+
+
+  onBlur (value) {
+    console.log('onBlur', value);
+  }
+
+
+  onChange (value) {
+    console.log('input changes to :' + value);
+  }
+
+
+  onSuggestSelect (suggest) {
+    console.log(suggest);
+  }
+
+
+  onSuggestNoResults (userInput) {
+    console.log('onSuggestNoResults for :' + userInput);
+  }
+
   render() {
     return (
       <div>
-        <Geosuggest 
+        <Geosuggest
+          placeholder="Sua pesquisa"
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          onChange={this.onChange}
+          onSuggestSelect={this.onSuggestSelect}
+          onSuggestNoResults={this.onSuggestNoResults}
+          location={new google.maps.LatLng(53.558572, 9.9278215)}
+          radius="20"
           />
       </div>
     );
