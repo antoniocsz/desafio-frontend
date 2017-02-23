5012ce7ddf6493eb776d03f2c0b8103253cdd8c1
Antonio Santana <antcesar.santana@gmail.com>
2017-02-23 15:54:20 -0300

    basico funcional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import Container from './Components/Container/Container';
+import Geosuggest from 'react-geosuggest';
+import axios from 'axios';
+
 
 class App extends Component {
+
+  state = {
+    ative : 0,
+    url : [],
+  };
+
+  onSuggestSelect (suggest) {
+    const key = 'rXfAB0lBZ6N5ol8xIGarJsQYNHyxek5NcnE2RN22';
+    axios.get(`https://api.nasa.gov/planetary/earth/imagery?lon=${suggest.location.lng}&lat=${suggest.location.lat}&api_key=${key}`).then((dados)=>{
+      this.setState({ url: dados.data.url, ative: 1 })
+    });
+  }
 
   render() {
     return (
       <div>
-        <Container />
-      </div>
+        <Geosuggest
+          placeholder="Sua pesquisa"
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          onChange={this.onChange}
+          onSuggestSelect={this.onSuggestSelect.bind(this)}
+          onSuggestNoResults={this.onSuggestNoResults}
+          />
+        {this.state.ative ?
+          <img
+            alt="imagem via satelite"
+            src={this.state.url}
+            />
+          :
+          <div></div>
+        }
+    </div>
     );
   }
 }
+
+
 
 export default App;
